07ba5b923db7386846d178e0d16230f95f7ddbb9
Cassidy <cjwang14@gmail.com>
2017-01-21 01:55:54 -0500

    Audio out

--- Sound Transfer/AudioOutTestViewController.swift
+++ Sound Transfer/AudioOutTestViewController.swift
@@ -7,31 +7,39 @@
 //
 
 import UIKit
+import AudioKit
 
 class AudioOutTestViewController: UIViewController {
     
     @IBOutlet weak var broadcastField: UITextField!
     @IBOutlet weak var playButton: UIButton!
-    @IBOutlet weak var listenButton: UIButton!
-    @IBOutlet weak var freqInLabel: UILabel!
+    
+    var oscillator = AKOscillator()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        AudioKit.output = oscillator
+        AudioKit.start()
     }
     
-    
-    @IBAction func playSound() {
-        if let freq = Int(broadcastField.text!) {
-            
+    @IBAction func toggleSound() {
+        if oscillator.isPlaying {
+            oscillator.stop()
+            playButton.setTitle("Start sound", for: .normal)
         } else {
+            oscillator.amplitude = 1
             
+            if let frequency = Double(broadcastField.text!) {
+                oscillator.frequency = frequency
+            } else {
+                oscillator.frequency = random(500, 5000)
+            }
+            oscillator.start()
+            playButton.setTitle("Stop sound", for: .normal)
         }
     }
     
-    @IBAction func listen() {
-    }
 
     /*
     // MARK: - Navigation
